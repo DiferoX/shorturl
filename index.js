80f2cd77357d8dae686b0d9b0c47683b8a702dd6
Diego <diferox@gmail.com>
2023-01-19 17:53:32 -0500

    add and edit home.ejs

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server PORT: " + PORT));
